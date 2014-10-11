40c7ed91d3ebe794045ec29998aa66e67b4579fe
Matt Gaunt <matt@gauntface.co.uk>
2014-10-11 10:01:55 +0100

    Initial commit

--- src/co/uk/gauntface/android/libs/utils/GFDateUtils.java
+++ src/co/uk/gauntface/android/libs/utils/GFDateUtils.java
@@ -1,7 +1,9 @@
 package co.uk.gauntface.android.libs.utils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,5 +17,15 @@ public class GFDateUtils {
     public static String formatCalendarForSQLite(Calendar calendar) {
         SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
         return dateFormatter.format(calendar.getTime());
+    }
+
+    public static Date getCalendarFromSqlite(String dateString) {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+        try {
+            return dateFormatter.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
